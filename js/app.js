5b02eb36cb3bb23404f56e55051f5d8a2d21d455
Bruce P Cox Jr <bpcoxjr@yahoo.com>
2015-08-18 13:00:47 -0400

    Latest commit

--- js/app.js
+++ js/app.js
@@ -8,14 +8,39 @@ $(document).ready(function () {
 			$('.ryu-still').show();
 		})
 		.mousedown(function() {
-			//play hadouken sound
+			playHadouken();
 			$('.ryu-ready').hide();
 			$('.ryu-throwing').show();
-			$('.hadouken').show();
-			//animate hadouken to the right of the screen
+			$('.hadouken').finish().show()
+				.animate(
+				{'left':'1020px'},
+				500,
+				function() {
+					$(this).hide();
+					$(this).css('left', '520px');
+				}
+			);
 		})
 		.mouseup(function() {
 			$('.ryu-throwing').hide();
 			$('.ryu-ready').show();
 		});
+
+		$(document).keydown(function(e) {
+			if (e.keyCode == 88) {
+				$('.ryu-action').hide();
+				$('.ryu-cool').show();
+			}
+		}).keyup(function(e) {
+			if (e.keyCode == 88) {
+				$('.ryu-cool').hide();
+				$('.ryu-still').show();
+			}
+		})
 });
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume= 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
